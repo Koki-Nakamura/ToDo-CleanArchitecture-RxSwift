ab3939642e57801f67c42048146068fbf8b31ba4
Koki Nakamura <koki.nakamura@nextinnovation-inc.co.jp>
2019-07-06 22:05:51 +0900

    ディレクトリなどの初期ファイル作成

---
+++ ToDo-CleanArchitecture-RxSwift/Presenter/MyPage/View/MyPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyPageViewController.swift
+//  ToDo-CleanArchitecture-RxSwift
+//
+//  Created by 中村 洸貴 on 2019/07/06.
+//  Copyright © 2019 中村 洸貴. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDo-CleanArchitecture-RxSwift/Presenter/MyPage/ViewModel/MyPageViewModelType.swift
@@ -0,0 +1,9 @@
+//
+//  MyPageViewModelType.swift
+//  ToDo-CleanArchitecture-RxSwift
+//
+//  Created by 中村 洸貴 on 2019/07/06.
+//  Copyright © 2019 中村 洸貴. All rights reserved.
+//
+
+import Foundation
